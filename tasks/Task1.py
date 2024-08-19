00bf5fb085c27a18271a801f26e6bd119dc4a3c1
karthikmadhava <karthikmadhava92@gmail.com>
2024-08-19 12:10:32 +0530

    Task 1

---
+++ tasks/Task1.py
@@ -0,0 +1,24 @@
+# Task #1
+
+# # Home Can you create a Program I will give you number(userinput and print table)
+
+# f"{}" String format concept
+# User input - num int -> 10, 100, -1, 2, 3.14 -> input
+# 9x1 = 9
+# 9x2 = 18... till 10
+
+
+customNum = int(input("Enter the Number :"))
+print(f"{customNum}*1={customNum * 1}")
+print(f"{customNum}*2={customNum * 2}")
+print(f"{customNum}*3={customNum * 3}")
+print(f"{customNum}*4={customNum * 4}")
+print(f"{customNum}*5={customNum * 5}")
+print(f"{customNum}*6={customNum * 6}")
+print(f"{customNum}*7={customNum * 7}")
+print(f"{customNum}*8={customNum * 8}")
+print(f"{customNum}*9={customNum * 9}")
+print(f"{customNum}*10={customNum * 10}")
+
+
+
