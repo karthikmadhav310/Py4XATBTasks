b4f987332f4772d1a681bb3073dedca1918934b9
karthikmadhava <karthikmadhava92@gmail.com>
2024-08-21 20:52:44 +0530

    Task 3

--- tasks/Task3.py
+++ tasks/Task3.py
@@ -3,21 +3,28 @@
 #
 #
 # - Explain the difference between the = operator and the == operator in Python.
-#
-# - What does the ** operator do in Python, and how is it used?
-#
-# - What does the ^ operator do in Python, and in what context is it commonly used?
-#
 
-#Difference between assignment operator and
 # variable_name = variable_value
 # Identifier = literals
 
-Karthik = 31
-
-
-
+# = Assignment Operator
+#Karthik = 31
 
 #  == Compare operator
+#K == 14
+
+
+
+# - What does the ** operator do in Python, and how is it used?
+# ** is used for exponential
+
+
+# - What does the ^ operator do in Python, and in what context is it commonly used?
+
+#^ bit wise operatpor - Like used in digital electronics true or false operations.
+
+
+
+
 
 
