62d1588be21fd81d6c646025e1ac2e3a0b467f34
karthikmadhava <karthikmadhava92@gmail.com>
2024-08-22 01:29:58 +0530

    Task 6

--- tasks/Task6.py
+++ tasks/Task6.py
@@ -1,3 +1,17 @@
 # ### Task #6
 #
 # - Develop a Python script that calculates the square and cube of a given number.
+
+number = int(input("Enter the Number to calculate the value :"))
+square = number ** 2
+cube = number ** 3
+
+# print(f"The Square of the entered number {number} is :{square}")
+
+print(f"The Square of {number} is {square}")
+
+print(f"The Cube of {number} is {cube}")
+
+
+
+
