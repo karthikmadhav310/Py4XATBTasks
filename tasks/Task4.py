58d243ecb3deb7a31cd0eb39bc7f13c81aa57f65
karthikmadhava <karthikmadhava92@gmail.com>
2024-08-22 01:03:52 +0530

    Task 4

--- tasks/Task4.py
+++ tasks/Task4.py
@@ -1,9 +1,10 @@
 # ### Task #4
-#
-#
-#
+
 # - Write a Python program to calculate the area of a circle given
 # its radius using the formula ``` area=π×r^2``` ( Take pie as 3.14)
 
-
+pi = 3.14
+radius = float(input("Enter the radius of the circle : "))
+area = pi * radius **2
+print(f"The are of the circle with radius {radius} is {area :.2f} square units.")
 
